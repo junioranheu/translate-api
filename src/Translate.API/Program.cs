abc9d5336fa3d5413afc64273fcd2d5165fb0a82
junioranheu <junioranheu@gmail.com>
2024-02-28 15:28:58 -0300

    estruturar API

---
+++ src/Translate.API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Translate.API");
+        c.RoutePrefix = "";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ src/Translate.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Translate.Application;
+
+public class Class1
+{
+
+}

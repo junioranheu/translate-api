27f5b8ae2b0912d9c2f770dd13da3cd9771381a3
junioranheu <junioranheu@gmail.com>
2024-02-29 12:15:03 -0300

    🛠️ refatorar Program.cs & implementar UseAppConfiguration

---
+++ src/Translate.API/DependencyAppConfiguration.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Translate.Domain.Consts;
+
+namespace Translate.API;
+
+public static class DependencyAppConfiguration
+{
+    public static WebApplication UseAppConfiguration(this WebApplication app, WebApplicationBuilder builder)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        IServiceProvider services = scope.ServiceProvider;
+
+        AddSwagger(app);
+        AddHttpsRedirection(app);
+        AddCors(app, builder);
+        AddCompression(app);
+        AddAuth(app);
+
+        return app;
+    }
+
+    private static void AddSwagger(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{SistemaConst.NomeSistema}.API");
+                // c.RoutePrefix = ""; // ***
+                c.DocExpansion(DocExpansion.None);
+            });
+
+            app.UseDeveloperExceptionPage();
+        }
+    }
+
+    private static void AddHttpsRedirection(WebApplication app)
+    {
+        if (app.Environment.IsProduction())
+        {
+            app.UseHttpsRedirection();
+        }
+    }
+
+    private static void AddCors(WebApplication app, WebApplicationBuilder builder)
+    {
+        app.UseCors(builder.Configuration["CORSSettings:Cors"]!);
+    }
+
+    private static void AddCompression(WebApplication app)
+    {
+        /// <summary>
+        /// O trecho "app.UseWhen" abaixo é necessário quando a API tem uma resposta IAsyncEnumerable/Yield;
+        /// O "UseResponseCompression" conflita com esse tipo de requisição, portanto é obrigatória a verificação abaixo;
+        /// Caso não existam requisições desse tipo na API, é apenas necessário o trecho "app.UseResponseCompression()";
+        /// </summary>
+        app.UseWhen(context => !IsStreamingRequest(context), x =>
+        {
+            x.UseResponseCompression();
+        });
+
+        static bool IsStreamingRequest(HttpContext context)
+        {
+            Endpoint? endpoint = context.GetEndpoint();
+
+            if (endpoint is RouteEndpoint routeEndpoint)
+            {
+                ControllerActionDescriptor? acao = routeEndpoint.Metadata.GetMetadata<ControllerActionDescriptor>();
+
+                if (acao is not null)
+                {
+                    Type? tipo = acao.MethodInfo.ReturnType;
+
+                    if (tipo.IsGenericType && tipo.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private static void AddAuth(WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
--- src/Translate.API/Program.cs
+++ src/Translate.API/Program.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Mvc.Controllers;
-using Swashbuckle.AspNetCore.SwaggerUI;
 using Translate.API;
 using Translate.Application;
 using Translate.Domain;
-using Translate.Domain.Consts;
 using Translate.Infrastructure;
 
-#region builder
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddDependencyInjectionAPI();
@@ -14,74 +10,9 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
     builder.Services.AddDependencyInjectionDomain();
     builder.Services.AddDependencyInjectionInfrastructure(builder);
 }
-#endregion
 
-#region app
 WebApplication app = builder.Build();
 {
-    using IServiceScope scope = app.Services.CreateScope();
-    IServiceProvider services = scope.ServiceProvider;
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{SistemaConst.NomeSistema}.API");
-            // c.RoutePrefix = ""; // ***
-            c.DocExpansion(DocExpansion.None);
-        });
-
-        app.UseDeveloperExceptionPage();
-    }
-
-    if (app.Environment.IsProduction())
-    {
-        app.UseHttpsRedirection();
-    }
-
-    app.UseCors(builder.Configuration["CORSSettings:Cors"]!);
-
-    /// <summary>
-    /// O trecho "app.UseWhen" abaixo é necessário quando a API tem uma resposta IAsyncEnumerable/Yield;
-    /// O "UseResponseCompression" conflita com esse tipo de requisição, portanto é obrigatória a verificação abaixo;
-    /// Caso não existam requisições desse tipo na API, é apenas necessário o trecho "app.UseResponseCompression()";
-    /// </summary>
-    app.UseWhen(context => !IsStreamingRequest(context), x =>
-    {
-        x.UseResponseCompression();
-    });
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-
+    app.UseAppConfiguration(builder);
     app.Run();
 }
-#endregion
-
-#region metodos_auxiliares
-static bool IsStreamingRequest(HttpContext context)
-{
-    Endpoint? endpoint = context.GetEndpoint();
-
-    if (endpoint is RouteEndpoint routeEndpoint)
-    {
-        ControllerActionDescriptor? acao = routeEndpoint.Metadata.GetMetadata<ControllerActionDescriptor>();
-
-        if (acao is not null)
-        {
-            Type? tipo = acao.MethodInfo.ReturnType;
-
-            if (tipo.IsGenericType && tipo.GetGenericTypeDefinition() == typeof(IAsyncEnumerable<>))
-            {
-                return true;
-            }
-
-            return false;
-        }
-    }
-
-    return false;
-}
-#endregion
